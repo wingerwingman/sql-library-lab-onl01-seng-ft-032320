42fff916ab16f30e6ab73091bc676055ac17a77d
Justin Winger <justinwinger4@gmail.com>
2020-04-20 19:27:40 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
-INSERT INTO series (title, author_id, subgenre_id) values ("bond", 2, 4), ("narly", 3, 2);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("bond", 2, 4), ("narly", 3, 2);
+
+INSERT INTO subgenras (name) VALUES ("Indie"), ("phyphi");
+
+INSERT INTO authors (name) VALUES ("Dan"), ("Andy");
+
+INSERT INTO books (title, year, series_id) VALUES ("Depression", 1979, 1), ("Flabo", 1999, 1), ("Clamado", 2009, 1), ("Clariton", 1978, 2), ("Clam", 1077, 2), ("Crazy", 1009, 2);
+
