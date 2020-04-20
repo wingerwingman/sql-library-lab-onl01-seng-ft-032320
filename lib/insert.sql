f48b0b338483d445e0bcac147e372e4f1e2544f0
Justin Winger <justinwinger4@gmail.com>
2020-04-20 19:45:32 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,4 +8,4 @@ INSERT INTO books (title, year, series_id) VALUES ("Depression", 1979, 1), ("Fla
 
 INSERT INTO characters (name, species, motto, author_id) VALUES ("Bond", "human", "shaken not stired", 1), ("Tim", "alien", "help", 1), ("prince", "prince", "I own you", 1), ("Leihah", "princes", "none shall pass", 1), ("tim", "carrot", "grow", 2), ("Ashley", "Girl", "WAAA", 2), ("Haily", "hip hop", "Raping", 2), ("Felicia", "Whinner", "Good Bye", 2);
 
-INSERT INTO character_books (book_id, character_id) VALUES (1, 1), (1, 2), (1, 3), (1, 4), (2, 5), (2, 6), (2, 7), (2,8);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 1), (1, 2), (1, 3), (1, 4), (2, 5), (2, 6), (2, 7), (2,8), (1, 5), (1,6), (2,1), (2, 2), (2, 3), (1, 7);
