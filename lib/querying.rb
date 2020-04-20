849098b56c91b119598c396a4d2c42b525d54f14
Justin Winger <justinwinger4@gmail.com>
2020-04-20 20:38:34 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series 
   JOIN characters ON series.id = characters.series_id 
-  WHERE characters.species = 'human' LIMIT 1;"
+  WHERE characters.species = 'human' LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
