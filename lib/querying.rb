ead308cdc06feaf6f353fec5fafa5f6ccd336605
Justin Winger <justinwinger4@gmail.com>
2020-04-20 20:06:36 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -7,7 +7,11 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT character.name, characters.motto FROM characters
+    JOIN series 
+    on books.series_id = series.id
+    WHERE series.id = 1 
+    ORDER BY books.year;"
 end
 
 
