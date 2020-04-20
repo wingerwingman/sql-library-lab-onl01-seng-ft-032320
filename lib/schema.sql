808d131a6969d818a3a78f0834c0dc5ae6570dc7
Justin Winger <justinwinger4@gmail.com>
2020-04-20 18:49:09 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -5,6 +5,11 @@ CREATE TABLE series (
   subgenre_id INTEGER
   );
   
+  CREATE TABLE subgenres (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+  );
+  
   CREATE TABLE authors (
     id INTEGER PRIMARY KEY,
     name TEXT
