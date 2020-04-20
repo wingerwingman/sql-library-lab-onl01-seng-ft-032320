81f98e13d43cfe21e7e38664397b09df5a009f0a
Justin Winger <justinwinger4@gmail.com>
2020-04-20 20:30:40 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, series.subgenre_id FROM authors
+  "SELECT authors.name, subgenres.name FROM series
     INNER JOIN authors
     ON series.author_id = authors.id
     INNER JOIN subgenres
