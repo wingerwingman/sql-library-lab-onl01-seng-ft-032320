2980c30b6a6cab30c90a3899186ce717815dcd5a
Justin Winger <justinwinger4@gmail.com>
2020-04-20 20:16:33 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, MAX(characters.species) FROM characters"
+  "SELECT MAX(characters.species), SUM(characters.species) FROM characters"
 end
 
 def select_name_and_series_subgenres_of_authors
