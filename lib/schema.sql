bd3d6022f0e2d9b6c05b0a645458cf0bd6051b52
Justin Winger <justinwinger4@gmail.com>
2020-04-20 18:45:15 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,3 +4,29 @@ CREATE TABLE series (
   author_id INTEGER,
   subgenre_id INTEGER
   );
+  
+  CREATE TABLE authors (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+  );
+  
+  CREATE TABLE books (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    year INTEGER,
+    series_id INTEGER 
+  );
+  
+  CREATE TABLE table (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    spedies TEXT,
+    motto TEXT,
+    author_id INTEGER
+  );
+  
+  CREATE TABLE caracter_books (
+    id INTEGER PRIMARY KEY,
+    book_id INTEGER,
+    caracter_id INTEGER,
+  );
