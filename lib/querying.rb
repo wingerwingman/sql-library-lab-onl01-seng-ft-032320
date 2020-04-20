420394682d1a0b0240ce6a71ffe2ee37ae06ae7c
Justin Winger <justinwinger4@gmail.com>
2020-04-20 20:36:34 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,12 +25,14 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM characters 
-    INNER JOIN series
-    ON characters.series_id = series.id 
-    GROUP BY series.title, species 
-    HAVING characters.species = 'human'
-    ORDER BY COUNT(species) DESC LIMIT 1;"
+  "SELECT Series.title
+  FROM characters
+  INNER JOIN series
+  ON characters.series_id = series.id
+  GROUP BY Series.title, species
+  HAVING characters.species = 'human'
+  ORDER BY COUNT(species) DESC
+  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
