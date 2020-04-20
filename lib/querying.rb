b3b0f709814cb9d143204a5fa668acd9f52225cd
Justin Winger <justinwinger4@gmail.com>
2020-04-20 20:12:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto FROM characters
-    ORDER BY LENGTH(characters.motto) DESC;"
+    ORDER BY LENGTH(characters.motto) DESC LIMIT 1;"
 end
 
 
