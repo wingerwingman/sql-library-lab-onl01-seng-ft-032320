1cd92909b52cb7e5a320d3614ac0d1424037c1e2
Justin Winger <justinwinger4@gmail.com>
2020-04-20 20:10:44 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto FROM characters
-    ORDER BY MAX(characters.motto);"
+    ORDER BY LENGTH(characters.motto);"
 end
 
 
