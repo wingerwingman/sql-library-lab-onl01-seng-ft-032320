b12f208fd6530125a361dc6cb19b5996cd89079c
Justin Winger <justinwinger4@gmail.com>
2020-04-20 20:26:06 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,7 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, series.subgenres_id FROM authors
     JOIN series
-    ON series.subgenres_id = subgenres.id"
+    ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
